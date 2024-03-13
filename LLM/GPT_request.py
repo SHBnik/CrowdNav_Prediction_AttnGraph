b4d5059678a9a1449aa259096d1621d5b9c04856
Heman <64802671+Heman15@users.noreply.github.com>
2024-03-13 12:17:40 -0700

    Update GPT_request.py
    Model changed

--- LLM/GPT_request.py
+++ LLM/GPT_request.py
@@ -1,26 +1,18 @@
 from openai import OpenAI
 import LLM.My_Key as My_Key
 
-
 class GPT:
     def __init__(self):
         self.client = OpenAI(api_key=My_Key.key)
 
     def ask(self, prompt):
         response = self.client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
+            model="text-davinci-002",  # Specify LLMA model here
             response_format={"type": "json_object"},
             messages=[
                 {
                     "role": "system",
-                    "content": "there are N humans and 1 robot in a 2D map. "
-                    "humans move in random trjectories in the map which it can collide with the robot."
-                    "you will get state of each human and the robot in this format "
-                    "[position x, positon y, velocity x,velocity y,theta] also next 5 predicted trajectory"
-                    " in a 5x1 list format of [position x, position y] and you should generate"
-                    " likelihood of collision for each human with the robot. output should be json"
-                    " with format of id number of each human is the key and the value of that key is the likelihood"
-                    " of colision of that human with the robot in percentage.",
+                    "content": "There are N humans and 1 robot in a 2D map... (Your prompt)",
                 },
                 {"role": "user", "content": prompt},
             ],
